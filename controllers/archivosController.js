669930aabf1aa0edb460147120ca16f7b915e1cf
Daniel Mateu <danielmateu86@gmail.com>
2023-02-03 21:41:44 +0100

    Creando routing y controllers de archivos

---
+++ controllers/archivosController.js
@@ -0,0 +1,9 @@
+
+exports.subirArchivo = async(req, res, next) => {
+
+}
+
+
+exports.eliminarArchivos = async(req, res, next) => {
+
+}
---
+++ routes/archivos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const archivosController = require('../controllers/archivosController')
+// const { check } = require('express-validator')
+const auth = require('../middleware/auth');
+
+router.post('/',
+    archivosController.subirArchivo
+)
+
+router.delete('/:id',
+    archivosController.eliminarArchivos
+)
+
+module.exports = router
+
