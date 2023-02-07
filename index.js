4c3e10a2eda1d9bbd6e5d18fd82837c530037855
Daniel Mateu <danielmateu86@gmail.com>
2023-02-07 18:28:55 +0100

    a ver...

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ const opcionesCors = {
 }
 app.use(cors(opcionesCors));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 //Puerto de la app
 const port = process.env.PORT || 4000;
 
