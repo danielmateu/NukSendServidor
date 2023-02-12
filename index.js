fe6cf3d08677a2e4e6e5c6ffdb5b04880f20147c
Daniel Mateu <danielmateu86@gmail.com>
2023-02-12 21:31:16 +0100

    second chance...

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ const opcionesCors = {
 app.use(cors(opcionesCors));
 
 app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "https://nuksend-front.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // res.header('Access-Control-Allow-Origin', '*');
     // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     // res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
 });
 
