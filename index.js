f1b2dfb99cd4e12485f42c3ad8ef523e53cb6635
Daniel Mateu <danielmateu86@gmail.com>
2023-02-12 22:08:12 +0100

    another change...

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ app.use(cors(opcionesCors));
 
 app.use(function (req, res, next) {
 
-    // res.header("Access-Control-Allow-Origin", "https://nuksend-front.vercel.app");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "https://nuksend-front.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
 
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     // res.header("Access-Control-Allow-Origin", "*");
     // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
